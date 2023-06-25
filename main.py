6d3f2f90055619fa64a97607ea53585d5b7a4661
Ricardo Groth <ricardogrothsilva@hotmail.com>
2023-06-25 19:21:33 -0300

    musica de fundo

--- main.py
+++ main.py
@@ -20,21 +20,23 @@ estrelas = []
 def abrir_caixa_de_pergunta(posicao):
     root = tk.Tk()
     root.withdraw()
-    resposta = simpledialog.askstring("Pergunta", "Qual é o nome da estrela?")
-    if resposta:
-        estrelas.append((posicao, resposta))
+    resposta = simpledialog.askstring("Pergunta", "Qual é o nome da estrela? \nCaso não saiba clique em Cancel")
+    if resposta is None or resposta.strip() == "":
+        resposta = "desconhecido"
+    resposta += f" ({posicao[0]}, {posicao[1]})"
+    estrelas.append((posicao, resposta))
 
 def desenhar_estrelas():
     for posicao, nome in estrelas:
-        pygame.draw.circle(tela, (255, 0, 0), posicao, 5)
+        pygame.draw.circle(tela, (255, 255, 255), posicao, 5)
         fonte = pygame.font.Font(None, 20)
-        texto = fonte.render(nome, True, (255, 0, 0))
+        texto = fonte.render(nome, True, (255, 255, 255))
         tela.blit(texto, (posicao[0] + 10, posicao[1] - 10))
 
 def desenhar_linhas():
     if len(estrelas) >= 2:
         for i in range(len(estrelas) - 1):
-            pygame.draw.line(tela, (255, 0, 0), estrelas[i][0], estrelas[i+1][0], 2)
+            pygame.draw.line(tela, (255, 255, 255), estrelas[i][0], estrelas[i+1][0], 2)
 
 
 
@@ -43,6 +45,8 @@ def jogo():
     preto = (0,0,0)
     fundo = pygame.image.load("fundospace.jpg")
     running = True
+    pygame.mixer.music.load("Space_Machine_Power.mp3")
+    pygame.mixer.music.play(-1)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
