fdeb785c2f01943990d7edc7f39532c3eef8d6d5
Ricardo Groth <ricardogrothsilva@hotmail.com>
2023-06-25 20:05:36 -0300

    adicionando o botao f10 para salvar os ponto

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ def desenhar_estrelas():
         texto = fonte.render(nome, True, (255, 255, 255))
         tela.blit(texto, (posicao[0] + 10, posicao[1] - 10))
 
+def salvar_pontos_arquivo():
+    with open("pontos.txt", "w") as arquivo:
+        for posicao, nome in estrelas:
+            arquivo.write(f"{posicao[0]},{posicao[1]},{nome}\n")
+
+
 def desenhar_linhas():
     if len(estrelas) >= 2:
         for i in range(len(estrelas) - 1):
@@ -47,6 +53,10 @@ def jogo():
     running = True
     pygame.mixer.music.load("Space_Machine_Power.mp3")
     pygame.mixer.music.play(-1)
+    fonte = pygame.font.Font(None, 20)
+    texto1 = "Pressione F10 para Salvar os pontos"
+    texto_renderizado = fonte.render(texto1, True, branco)
+    posicao_texto = (260 // 2 - texto_renderizado.get_width() // 2, 15 // 2 - texto_renderizado.get_height() // 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -54,12 +64,16 @@ def jogo():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1: 
                     abrir_caixa_de_pergunta(event.pos)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F10:
+                    salvar_pontos_arquivo()
         
 
 
         tela.blit(fundo, (0,0))
         desenhar_linhas()
         desenhar_estrelas()
+        tela.blit(texto_renderizado, posicao_texto)
         pygame.display.flip()
         clock.tick(60)
 
